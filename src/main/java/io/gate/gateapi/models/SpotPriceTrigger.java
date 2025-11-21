0d3c7525bd37da78a30f416d136858b833840ea4
gate <developer@gate.com>
2025-11-21 10:35:32 +0000

    update to v7.1.29

--- src/main/java/io/gate/gateapi/models/SpotPriceTrigger.java
+++ src/main/java/io/gate/gateapi/models/SpotPriceTrigger.java
@@ -28,7 +28,7 @@ public class SpotPriceTrigger {
     private String price;
 
     /**
-     * Price trigger condition  - &#x60;&gt;&#x3D;&#x60;: triggered when market price is greater than or equal to &#x60;price&#x60; - &#x60;&lt;&#x3D;&#x60;: triggered when market price is less than or equal to &#x60;price&#x60; 
+     * 价格条件类型 - 大于等于: 表示市场价格大于等于 price 时触发 - 小于等于: 表示市场价格小于等于 price 时触发
      */
     @JsonAdapter(RuleEnum.Adapter.class)
     public enum RuleEnum {
@@ -109,7 +109,7 @@ public class SpotPriceTrigger {
     }
 
      /**
-     * Price trigger condition  - &#x60;&gt;&#x3D;&#x60;: triggered when market price is greater than or equal to &#x60;price&#x60; - &#x60;&lt;&#x3D;&#x60;: triggered when market price is less than or equal to &#x60;price&#x60; 
+     * 价格条件类型 - 大于等于: 表示市场价格大于等于 price 时触发 - 小于等于: 表示市场价格小于等于 price 时触发
      * @return rule
     **/
     public RuleEnum getRule() {
